e1315dacca5361071ab1b72e53be341c38c9e400
hekmatnajafi <najafihekmat@gmail.com>
2021-10-17 19:40:40 +0430

    changed input method from txt file to command line and removed input.txt file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,50 +1,44 @@
 package com.example;
 
+import java.util.Scanner;
 import java.util.Vector;
 import java.nio.file.*;
 
 public class App 
 {
-    // Reading input file from input.txt file
-    public static String readFile()throws Exception
-    {
-      String path = "/var/www/react/projects/Browze/demo/src/main/java/com/example/input.txt";
-      return new String(Files.readAllBytes(Paths.get(path)));
-    }
     public static void main( String[] args )throws Exception
     {
-        // Getting file data in main
-        String input = App.readFile();
-
-
         Vector<String> result = new Vector<String>();   
         int w;
         int h;
-        
-        // Storing/splitting data into array based on new line
-        String[] file = input.split("\\R");
-
         // extracting the Height and width of Platua
-        String platua = file[0].split(":")[1];
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Plateau:");
+        String platua = inp.nextLine();
         String[] data = platua.split(" ");
         w = Integer.parseInt(data[0]);
         h = Integer.parseInt(data[1]);
         String coordinates;
         String instructions;
+        int number =1;
+        System.out.print("Rover"+number+" Landing:");
+        coordinates = inp.nextLine();
 
-        // Calculation and movement for every Rover
-        for(int i =1; i<file.length; i+=2)
+        while(!coordinates.isEmpty())
         {
-            // storing the coordinates and instructions of River i
-            coordinates = file[i].split(":")[1];
-            instructions = file[i+1].split(":")[1];
-
+            //  storing the coordinates and instructions of River i
+            System.out.print("Rover"+number+" Instructions:");
+            instructions = inp.nextLine();
             // passing the coordinate and instructions to Rover to move
             Rover rover = new Rover(coordinates);
             rover.goTo(instructions, w, h);
             result.add(rover.getResult());
-        }
+            number++;
 
+            System.out.print("Rover"+number+" Landing:");
+            coordinates = inp.nextLine();
+
+        }
         // Printing the result
         for(int i = 0; i< result.size(); i++)
             {
